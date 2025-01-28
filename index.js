e952d625412462529814e201db109d31f0aecec5
Elvis Anthony <info.elvisanthony@gmail.com>
2025-01-28 08:57:57 +0100

    'second'

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const cancel = document.querySelector('.menu')
 const tap = document.querySelector('#main')
 const overlay = document.getElementById('overlay')
 
+
 ham.addEventListener('click', () => {
     items.classList.toggle('active')
     cancel.classList.toggle('close')
     tap.classList.toggle('dim')
-    overlay.classList.toggle('ative')
+    overlay.classList.toggle('active')
 })
 
-tap.addEventListener('click', ()=> {
+overlay.addEventListener('click', ()=> {
     items.classList.remove('active')
     cancel.classList.remove('close')
+    overlay.classList.remove('active')
 })
+
