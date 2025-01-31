dadbf64dbeac27c9be99889ac8357f06253e9679
Elvis Anthony <info.elvisanthony@gmail.com>
2025-01-31 09:23:25 +0100

    'btn'

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const ham = document.getElementById('menu')
 const items = document.getElementById('items')
 const cancel = document.querySelector('.menu')
-const tap = document.querySelector('#main')
 const overlay = document.getElementById('overlay')
 const listItem =  document.querySelector('.item-list')
 
+//Toggle hamburger menu
 ham.addEventListener('click', () => {
     items.classList.toggle('active')
     cancel.classList.toggle('close')
-    tap.classList.toggle('dim')
     overlay.classList.toggle('active')
 })
 
+//Tapping anywhere removes menu
 overlay.addEventListener('click', ()=> {
     items.classList.remove('active')
     cancel.classList.remove('close')
     overlay.classList.remove('active')
 })
 
-
+//remove menu when link is clicked
 function item () {
     items.classList.remove('active')
     cancel.classList.remove('close')
