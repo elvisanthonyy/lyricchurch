caffc932b1f7f89b87bc780fe5e0407d2170eca7
Elvis Anthony <info.elvisanthony@gmail.com>
2025-02-19 16:35:49 +0100

    'change_alot'

--- index.js
+++ index.js
@@ -1,52 +1,41 @@
 const ham = document.getElementById('menu');
-const items = document.querySelector('.items');
-const cancel = document.querySelector('.menu') ;
+const cancel = document.querySelector('.menu');
+const menu = document.querySelector('.nav-menu');
 const overlay = document.getElementById('overlay') ;
-const listItem =  document.querySelector('.item-list')
-const leader = document.getElementById('meet-leader')
-const navBar = document.getElementById('homee')
-const body = document.querySelector('body')
+const listItem =  document.querySelector('.item-list');
+const leader = document.getElementById('meet-leader');
+const navBar = document.getElementById('homee');
+const body = document.querySelector('body');
+const cancelHam = document.querySelector('.cancel');
+ 
 
-function remove() {
-    items.classList.remove('active')
-    cancel.classList.remove('close')
-    overlay.classList.remove('active')
-}
+function toggleMenu () {
+    menu.classList.toggle('active');
+    cancel.classList.toggle('active');
+    body.classList.toggle('active');
+};
+
+function removeMenu () {
+    menu.classList.remove('active');
+    body.classList.remove('active');
+    cancel.classList.remove('active');
+};
+
 
 //Toggle hamburger menu
 ham.addEventListener('click', () => {
-    items.classList.toggle('active')
-    cancel.classList.toggle('close')
-    overlay.classList.toggle('active')
-    body.classList.toggle('active')
-})
+    
+    toggleMenu();
+});
 
 //Tapping anywhere removes menu
 overlay.addEventListener('click', ()=> {
-    remove()
-    body.classList.remove('active')
-})
+    removeMenu();
+});
 
 //remove menu when link is clicked
 items.addEventListener('click', e => {
     if(e.target.matches('a')) {
-        remove()
+        removeMenu();
     }
-})
-
-
-/*window.onscroll = () => {
-    let navHeight = 150
-    let leaderPosition = leader.getBoundingClientRect()
-    if(leaderPosition.y < navHeight) {
-        navBar.classList.add('active')
-       //alert(alert(leaderPosition.y))
-    } else if (leaderPosition.y == 0) {
-        leader.classList.add('active')
-        
-    }  else {
-        navBar.classList.remove('active')
-        leader.classList.remove('active')
-    }
-
-}*/
+});
