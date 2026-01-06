194e664dac0e3e08ffffba5656dd7f60950e8069
elvisanthonyy <info.elvisanthony@gmail.com>
2026-01-06 11:10:54 +0100

    force dynamic

--- app/us/partner/page.tsx
+++ app/us/partner/page.tsx
@@ -1,6 +1,8 @@
 import PartnerMain from "@/app/components/partner/PartnerMain";
 import PartnerNav from "@/app/components/partner/PartnerNav";
 
+export const dynamic = "force-dynamic";
+
 const baseURL = process.env.BASE_URL;
 
 export const metadata = {
